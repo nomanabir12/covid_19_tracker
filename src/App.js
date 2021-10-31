62f36b050d27fdf23304372d0b5de99fa683e2c6
Noman Abir <nomanabir12>
2021-10-31 12:07:42 +0600

    update API

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import React from "react";
 // import CountryPicker from "./components/CountryPicker/CountryPicker";
 
 import { Cards, Chart, CountryPicker } from './components';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import { fetchData } from './api';
 
 class App extends React.Component {
+
+    async componentDidMount() {
+        const data = await fetchData();
+
+        console.log(data);
+    }
+    
+
     render() {
       return(
           <div className={styles.container}>
