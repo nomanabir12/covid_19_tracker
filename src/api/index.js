b5e6d38aaf48e9e49cb61660e3e1d5fefabf4f97
Noman Abir <nomanabir12>
2021-11-01 17:25:29 +0600

    set all countries"

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async() => {
-    try {
-      const { data: { confirmed, recovered, deaths, lastUpdate}}  = await axios.get(url);
-      
-      return {confirmed, recovered, deaths, lastUpdate, };
-    } catch (error) {
+export const fetchData = async () => {
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-    }
+    return { confirmed, recovered, deaths, lastUpdate, };
+  } catch (error) {
+
+  }
 }
 
 
@@ -21,11 +21,23 @@ export const fetchDailyData = async () => {
       confirmed: dailyData.confirmed.total,
       deaths: dailyData.deaths.total,
       date: dailyData.reportDate,
-        }));
+    }));
 
-    
+
     return modifiedData;
   } catch (error) {
-  
+    console.log(error);
+
   };
 };
+
+export const fetchCountries = async () => {
+
+  try {
+    const { data: { countries }} = await axios.get(`${url}/countries`);
+
+   return countries.map ((country) => country.name)
+  } catch (error) {
+    console.log(error);
+  }
+}
